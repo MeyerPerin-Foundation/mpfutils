dcac008587c22cb575901cbe97b322ea9c16768a
Lucas Meyer <lucas.meyer@microsoft.com>
2025-02-16 16:57:02 -0600

    Added SAS to Azs client

--- src/mpfutils/azstorage/azs_container.py
+++ src/mpfutils/azstorage/azs_container.py
@@ -1,23 +1,29 @@
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient, ContainerClient
 import logging
 import os
 
 logger = logging.getLogger("mpf-utils.azstorage")
 
 class AzsContainerClient:
-    def __init__(self, container_name: str, conn_str: str = None):
-        if not conn_str:
-            conn_str = os.getenv("AZSTORAGE_CONNECTION_STRING")
-        blob_service_client = BlobServiceClient.from_connection_string(conn_str)
-        self.client = blob_service_client.get_container_client(container_name)
+    def __init__(self, container_name: str, conn_str: str = None, sas_url: str = None):
         
-        self.blob_service_client = BlobServiceClient.from_connection_string(conn_str)
-
+        if not conn_str and not sas_url:
+            conn_str = os.getenv("AZSTORAGE_CONNECTION_STRING")
+        
+        if not sas_url:
+            logger.info("Using connection string to connect to Azure Storage")
+            blob_service_client = BlobServiceClient.from_connection_string(conn_str)
+            self.container_client = blob_service_client.get_container_client(container_name)
+        else:
+            # if there's a sas_url, it overrides the conn_str
+            logger.info("Using SAS URL to connect to Azure Storage")
+            self.container_client = ContainerClient.from_container_url(sas_url)
+        
     def upload_blob(self, blob_name, data, overwrite=True):
-        blob_client = self.client.get_blob_client(blob=blob_name)
+        blob_client = self.container_client.get_blob_client(blob=blob_name)
         blob_client.upload_blob(data, overwrite=overwrite)
         return blob_client.url
 
     def download_blob(self, container_name, blob_name):
-        blob_client = self.client.get_blob_client(blob=blob_name)
+        blob_client = self.container_client.get_blob_client(blob=blob_name)
         return blob_client.download_blob().readall()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,22 +1,27 @@
 from src.mpfutils.ai import OpenAIClient
 from src.mpfutils.cosmosdb import CosmosDBContainer
 from src.mpfutils.azstorage import AzsContainerClient
+import pytest
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO)
 
+@pytest.mark.skip(reason="Focusing on Azure Storage tests")
 def test_openai_client():
     c = OpenAIClient()
     capital = c.run_prompt("What is the capital of France?")
     assert "Paris" in capital
 
+@pytest.mark.skip(reason="Focusing on Azure Storage tests")
 def test_cosmosdb_client():
     c = CosmosDBContainer("posts", "schedules")
     items = c.run_query("SELECT * FROM c")
     assert len(items) > 10
 
 def test_azstorage_client():
-    c = AzsContainerClient("mpfutils-test")
+    sas_url = os.getenv("AZSTORAGE_TEST_SAS")
+    c = AzsContainerClient("mpfutils-test", sas_url=sas_url)
     url = c.upload_blob("test.txt", "This is a test")
     assert url.startswith("https://")
     data = c.download_blob("mpfutils-test", "test.txt")
